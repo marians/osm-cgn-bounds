afcb22e62523d0a18f99fda7ae668bcd3353e357
Marian Steinbach <marian@sendung.de>
2012-06-27 14:38:12 +0200

    Projektions-Parameter

--- config.py
+++ config.py
@@ -4,6 +4,9 @@
 # Pfade
 SRC = 'sourcedata'
 
+# Projektions-Beschreibung für pyproj bzw. proj.4
+PROJECTION = '+proj=tmerc +lat_0=0 +lon_0=9 +k=0.9996 +x_0=2500000 +y_0=0 +ellps=WGS84 +units=m +no_defs'
+
 # Namen der Shapefiles
 STADTBEZIRKE = 'Stadtbezirk'
 STADTTEILE = 'Stadtteil'
